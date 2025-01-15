3e14d0eb75dbb22d791a87c73d8089f67bf76dfa
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-15 17:11:28 +0900

    #33612　入力フォーム　「仕入経緯」ラジオボタン　初期値設定不具合

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -330,7 +330,7 @@ function subStockEditView($param)
 					for ($i = 0; $i < 6; $i++) {
 					?>
 						<br />
-						<input type="radio" name="how" value="<?php print $i + 1; ?>" <?php if ($param["how"] == $i + 1) print ' checked="checked"'; ?> /> <?php print fnHowName($i); ?>
+						<input type="radio" name="how" value="<?php print $i + 1; ?>" <?php if ($param["how"] == $i) print ' checked="checked"'; ?> /> <?php print fnHowName($i); ?>
 					<?php
 					}
 					?>
