723c1ed1263c0f71c79220cf83519ee592f7b93a
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-15 13:57:10 +0900

    DB接続

--- SRC/libDBInit.php
+++ SRC/libDBInit.php
@@ -2,10 +2,11 @@
 //
 //DB接続
 //
-function fnDbConnect() {
-	$conn = mysqli_connect( 'localhost', '*** userid ***', '*** password ***' ) or die('DB接続エラー1です。管理者にご報告をお願いいたします。');
-	mysqli_select_db( $conn, '*** db_name ***' ) or die('DB接続エラー2です。管理者にご報告をお願いいたします。');
-	mysqli_query( $conn, 'SET NAMES utf8');
+function fnDbConnect()
+{
+	$conn = mysqli_connect('localhost', 'root', 'proclimb') or die('DB接続エラー1です。管理者にご報告をお願いいたします。');
+	mysqli_select_db($conn, 'appraiser1') or die('DB接続エラー2です。管理者にご報告をお願いいたします。');
+	mysqli_query($conn, 'SET NAMES utf8');
 
-	return($conn);
+	return ($conn);
 }
