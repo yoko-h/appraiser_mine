f5bb69f75af96ec3cf9499ffe46add2a536eb9ca
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-17 15:26:43 +0900

    #33726　仕入更新画面 登録データが入力欄に表示されていない

--- SRC/libDBConnect.php
+++ SRC/libDBConnect.php
@@ -88,11 +88,11 @@ function fnNextNo($t)
 {
     $conn = fnDbConnect();
 
-    $sql = "SELECT MAX(".$t."NO) FROM TBL".$t;
+    $sql = "SELECT MAX(" . $t . "NO) FROM TBL" . $t;
     $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     if ($row[0]) {
-        $max = $row[0];
+        $max = $row[0] + 1;
     } else {
         $max = 1;
     }
