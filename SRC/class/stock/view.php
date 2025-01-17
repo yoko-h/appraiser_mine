679cb6cbc49b446d2806f6e398cba47c6f2c855c
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-17 11:28:08 +0900

    #33614　検索結果　ページ総数の表示なし

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -119,7 +119,7 @@ function subStockView($param)
 			return;
 		}
 
-		$sql = fnSqlStockList(1, $param);
+		$sql = fnSqlStockList(0, $param);
 		$res = mysqli_query($param["conn"], $sql);
 		$row = mysqli_fetch_array($res);
 
