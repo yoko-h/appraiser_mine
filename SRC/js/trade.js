64aa2665025806e596cd1cb14c54e6fb290c6248
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-23 14:29:01 +0900

    #34052　業者登録　新規登録ボタンを押しても反応しない

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -12,7 +12,7 @@ function fnTradeEditCheck() {
 		return;
 	}
 
-	tmp = form.nameYomi.value;
+	tmp = form.nameFuri.value;
 	if (tmp.length > 100) {
 		alert('業者名（よみ）は100文字以内で入力してください');
 		return;
@@ -73,7 +73,7 @@ function fnTradeEditCheck() {
 	}
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
-		form.act.value = 'tradeEditCompleta';
+		form.act.value = 'tradeEditComplete';
 		form.submit();
 	}
 }
