f35e6a298efd0271f47fb047cf5e4a3954fa47c2
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-23 15:55:46 +0900

    #34055　業者登録　削除ボタンを押下した後、一覧に遷移せず、DBのDELも変更されない

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -83,6 +83,7 @@ function fnTradeEditCheck() {
 function fnTradeDeleteCheck(no) {
 	if (confirm('削除します。よろしいですか？')) {
 		form.tradeNo.value = no;
+		form.act.value = 'tradeDelete';
 		form.submit();
 	}
 }
