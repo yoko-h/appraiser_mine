cba23ead266f50ce0f82e36e282feb2598c13d1e
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-01-28 13:44:28 +0900

    #34305　売主物件更新　更新するとすべての情報が同じ更新内容で変更されてしまう。

--- SRC/class/sell/model.php
+++ SRC/class/sell/model.php
@@ -111,6 +111,7 @@ function fnSqlSellUpdate($param)
     $sql .= ",PRICE = '" . $param["price"] . "'";
     $sql .= ",NOTE = '" . $param["note"] . "'";
     $sql .= ",UPDT = CURRENT_TIMESTAMP";
+    $sql .= " WHERE SELLNO = " . $param["sellNo"];
 
     return $sql;
 }
