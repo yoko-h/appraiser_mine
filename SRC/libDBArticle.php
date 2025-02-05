391cbb54ef7f87e034cdc77c04d714d93add9c89
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-02-05 15:30:04 +0900

    #34567　物件更新　削除ボタン押下　DBの削除フラグ設定不備により「1」→「0」に変更され削除できない。

--- SRC/libDBArticle.php
+++ SRC/libDBArticle.php
@@ -110,7 +110,7 @@ function fnSqlArticleInsert($articleNo, $article, $room, $keyPlace, $address, $a
 function fnSqlArticleDelete($articleNo)
 {
 	$sql  = "UPDATE TBLARTICLE";
-	$sql .= " SET DEL = 0";
+	$sql .= " SET DEL = -1";
 	$sql .= ",UPDT = CURRENT_TIMESTAMP";
 	$sql .= " WHERE ARTICLENO = '$articleNo'";
 
