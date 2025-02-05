f4fd0b70993e09de325a090224db83523a4f4f39
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-02-05 15:47:18 +0900

    #34563　物件登録　削除ボタンが表示されている。

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -254,7 +254,9 @@ function subArticleEdit()
 
 		<a href="javascript:fnArticleEditCheck();"><img src="./images/<?php print $btnImage ?>" /></a>　
 		<a href="javascript:form.act.value='articleSearch';form.submit();"><img src="./images/btn_return.png" /></a>
-		&nbsp;&nbsp;<a href="javascript:fnArticleDeleteCheck(<?php print $articleNo ?>);"><img src="./images/btn_del.png" /></a>
+		<?php if ($articleNo) { ?>
+			&nbsp;&nbsp;<a href="javascript:fnArticleDeleteCheck(<?php print $articleNo ?>);"><img src="./images/btn_del.png" /></a>
+		<?php } ?>
 	</form>
 <?php
 }
