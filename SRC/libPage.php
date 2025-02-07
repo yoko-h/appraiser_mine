b7bb69cdca3dd088a6f0405737b2747f7d31c655
yoko_horikiri <202410ap01_horikiri@x-ide.sakura.ne.jp>
2025-02-07 16:05:38 +0900

    #34912　工事管理一覧　ソート機能　工期のソートが機能しない。

--- SRC/libPage.php
+++ SRC/libPage.php
@@ -2,60 +2,61 @@
 //
 //ページ処理
 //
-function fnPage($c,$p,$a){
-	$pages = (int)(($c - 1) / PAGE_MAX ) + 1;
+function fnPage($c, $p, $a)
+{
+	$pages = (int)(($c - 1) / PAGE_MAX) + 1;
 
-	if($pages < $p){
-		$p--;
+	if ($pages < $p) {
+		$p = $pages;
 	}
 
-	if($p > 1 && $pages > 1){
+	if ($p > 1 && $pages > 1) {
 ?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.sPage.value=<?php print $p - 1;?>;form.submit();">前へ</a> |
-<?php
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.sPage.value=<?php print $p - 1; ?>;form.submit();">前へ</a> |
+		<?php
 	}
 
-	if($p - 4 < 1 || $pages < 10){
+	if ($p - 4 < 1 || $pages < 10) {
 		$min = 1;
-	}else{
-		if($pages - $p < 4){
+	} else {
+		if ($pages - $p < 4) {
 			$min = $pages - 7;
-		}else{
+		} else {
 			$min = $p - 4;
 		}
 	}
 
-	if($p + 4 > $pages || $pages < 10){
+	if ($p + 4 > $pages || $pages < 10) {
 		$max = $pages;
-	}else{
-		if($p < 5){
+	} else {
+		if ($p < 5) {
 			$max = 9;
-		}else{
+		} else {
 			$max = $p + 4;
 		}
 	}
 
-	for($i = $min;$i <= $max;$i++){
-		if($p == $i){
-?>
-<?php print $i;?> |
-<?php
-		}else{
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.sPage.value=<?php print $i;?>;form.submit();"><?php print $i;?></a> |
-<?php
+	for ($i = $min; $i <= $max; $i++) {
+		if ($p == $i) {
+		?>
+			<?php print $i; ?> |
+		<?php
+		} else {
+		?>
+			<a href="javascript:form.act.value='<?php print $a; ?>';form.sPage.value=<?php print $i; ?>;form.submit();"><?php print $i; ?></a> |
+		<?php
 		}
 	}
 
-	if($p < $pages){
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.sPage.value=<?php print $p + 1;?>;form.submit();">次へ</a>
-<?php
+	if ($p < $pages) {
+		?>
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.sPage.value=<?php print $p + 1; ?>;form.submit();">次へ</a>
+	<?php
 	}
-?>
-検索結果：<?php print $c;?>件<br />
-<?php
-	return($p);
+	?>
+	検索結果：<?php print $c; ?>件<br />
+	<?php
+	return ($p);
 }
 
 
@@ -64,25 +65,26 @@ function fnPage($c,$p,$a){
 //
 //オーダー処理
 //
-function fnOrder($n,$a){
-	if($_REQUEST['orderBy'] == $n && $_REQUEST['orderTo'] == 'asc'){
-?>
-<span class="red">▲</span>
-<?php
-	}else{
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.orderBy.value='<?php print $n;?>';form.orderTo.value='asc';form.sPage.value=1;form.submit();">▲</a>
-<?php
+function fnOrder($n, $a)
+{
+	if ($_REQUEST['orderBy'] == $n && $_REQUEST['orderTo'] == 'asc') {
+	?>
+		<span class="red">▲</span>
+	<?php
+	} else {
+	?>
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.orderBy.value='<?php print $n; ?>';form.orderTo.value='asc';form.sPage.value=1;form.submit();">▲</a>
+	<?php
 	}
 
-	if($_REQUEST['orderBy'] == $n && $_REQUEST['orderTo'] == 'desc'){
-?>
-<span class="red">▼</span>
-<?php
-	}else{
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.orderBy.value='<?php print $n;?>';form.orderTo.value='desc';form.sPage.value=1;form.submit();">▼</a>
-<?php
+	if ($_REQUEST['orderBy'] == $n && $_REQUEST['orderTo'] == 'desc') {
+	?>
+		<span class="red">▼</span>
+	<?php
+	} else {
+	?>
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.orderBy.value='<?php print $n; ?>';form.orderTo.value='desc';form.sPage.value=1;form.submit();">▼</a>
+	<?php
 	}
 }
 
@@ -92,24 +94,25 @@ function fnOrder($n,$a){
 //
 //オーダー処理（工事管理表：工期用）
 //
-function fnOrderConstWork($n1,$n2,$a){
-	if($_REQUEST['orderBy'] == $n1){
-?>
-<span class="red">▲</span>
-<?php
-	}else{
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.orderBy.value='<?php print $n1;?>';form.orderTo.value='asc';form.sPage.value=1;form.submit();">▲</a>
-<?php
+function fnOrderConstWork($n1, $n2, $a)
+{
+	if ($_REQUEST['orderBy'] == $n1) {
+	?>
+		<span class="red">▲</span>
+	<?php
+	} else {
+	?>
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.orderBy.value='<?php print $n1; ?>';form.orderTo.value='asc';form.sPage.value=1;form.submit();">▲</a>
+	<?php
 	}
 
-	if($_REQUEST['orderBy'] == $n2){
-?>
-<span class="red">▼</span>
-<?php
-	}else{
-?>
-<a href="javascript:form.act.value='<?php print $a;?>';form.orderBy.value='<?php print $n2;?>';form.orderTo.value='asc';form.sPage.value=1;form.submit();">▼</a>
+	if ($_REQUEST['orderBy'] == $n2) {
+	?>
+		<span class="red">▼</span>
+	<?php
+	} else {
+	?>
+		<a href="javascript:form.act.value='<?php print $a; ?>';form.orderBy.value='<?php print $n2; ?>';form.orderTo.value='desc';form.sPage.value=1;form.submit();">▼</a>
 <?php
 	}
 }
@@ -120,11 +123,12 @@ function fnOrderConstWork($n1,$n2,$a){
 //
 //
 //
-function fnAuthorityName($n){
+function fnAuthorityName($n)
+{
 	$tmp[0] = '管理者';
 	$tmp[1] = '一般';
 
-	return($tmp[$n]);
+	return ($tmp[$n]);
 }
 
 
@@ -133,8 +137,9 @@ function fnAuthorityName($n){
 //
 //
 //
-function fnRankName($n){
-	return(substr('ABCDEFGHIJKLMNOPQRSTUVWXY ',$n,1));
+function fnRankName($n)
+{
+	return (substr('ABCDEFGHIJKLMNOPQRSTUVWXY ', $n, 1));
 }
 
 
@@ -143,13 +148,14 @@ function fnRankName($n){
 //
 //
 //
-function fnDistanceName($n){
+function fnDistanceName($n)
+{
 	$tmp[0] = 'A（5分以内）';
 	$tmp[1] = 'B（10分以内）';
 	$tmp[2] = 'C（15分以内）';
 	$tmp[3] = 'D（15分以上）';
 
-	return($tmp[$n]);
+	return ($tmp[$n]);
 }
 
 
@@ -158,7 +164,8 @@ function fnDistanceName($n){
 //
 //
 //
-function fnHowName($n){
+function fnHowName($n)
+{
 	$tmp[0] = '会社案件';
 	$tmp[1] = '新規業者（TEL,FAX）';
 	$tmp[2] = '新規業者（訪問）';
@@ -166,7 +173,7 @@ function fnHowName($n){
 	$tmp[4] = '既存業者（契約無）';
 	$tmp[5] = 'その他';
 
-	return($tmp[$n]);
+	return ($tmp[$n]);
 }
 
 
@@ -175,13 +182,14 @@ function fnHowName($n){
 //
 //
 //
-function fnConstFlgName($n){
+function fnConstFlgName($n)
+{
 	$tmp[0] = '工事前';
 	$tmp[1] = '工事中';
 	$tmp[2] = '工事終了';
 	$tmp[3] = '物件終了';
 
-	return($tmp[$n]);
+	return ($tmp[$n]);
 }
 
 
@@ -190,12 +198,13 @@ function fnConstFlgName($n){
 //
 //
 //
-function fnContentName($n){
+function fnContentName($n)
+{
 	$tmp[0] = '案内';
 	$tmp[1] = 'OP';
 	$tmp[2] = '下見';
 
-	return($tmp[$n]);
+	return ($tmp[$n]);
 }
 
 
@@ -204,10 +213,11 @@ function fnContentName($n){
 //
 //
 //
-function fnToFormYMD($ymd){
-	if($ymd){
-		return(str_replace('-','/',substr($ymd,0,10)));
-	}else{
+function fnToFormYMD($ymd)
+{
+	if ($ymd) {
+		return (str_replace('-', '/', substr($ymd, 0, 10)));
+	} else {
 		return;
 	}
 }
@@ -218,10 +228,11 @@ function fnToFormYMD($ymd){
 //
 //
 //
-function fnToFormH($h){
-	if($h){
-		return(substr($h,11,2));
-	}else{
+function fnToFormH($h)
+{
+	if ($h) {
+		return (substr($h, 11, 2));
+	} else {
 		return;
 	}
 }
@@ -232,10 +243,11 @@ function fnToFormH($h){
 //
 //
 //
-function fnToFormM($m){
-	if($m){
-		return(substr($m,14,2));
-	}else{
+function fnToFormM($m)
+{
+	if ($m) {
+		return (substr($m, 14, 2));
+	} else {
 		return;
 	}
 }
@@ -246,12 +258,13 @@ function fnToFormM($m){
 //
 //
 //
-function fnToSqlYMDHM($ymd,$h,$i){
-	if($ymd <> '' && $h <> '' && $i <> ''){
-		$h = substr('00'.$h,-2);
-		$i = substr('00'.$i,-2);
-		return($ymd." $h:$i:00");
-	}else{
+function fnToSqlYMDHM($ymd, $h, $i)
+{
+	if ($ymd <> '' && $h <> '' && $i <> '') {
+		$h = substr('00' . $h, -2);
+		$i = substr('00' . $i, -2);
+		return ($ymd . " $h:$i:00");
+	} else {
 		return;
 	}
 }
@@ -262,16 +275,17 @@ function fnToSqlYMDHM($ymd,$h,$i){
 //
 //
 //
-function fnNumFormat($n){
+function fnNumFormat($n)
+{
 	$y = '';
-	if($n){
-		while(strlen($n) > 3){
-			$y = ','.substr($n,-3,3).$y;
-			$n = substr($n,0,strlen($n) - 3);
+	if ($n) {
+		while (strlen($n) > 3) {
+			$y = ',' . substr($n, -3, 3) . $y;
+			$n = substr($n, 0, strlen($n) - 3);
 		}
-		$y = $n.$y;
+		$y = $n . $y;
 	}
 
-	return($y);
+	return ($y);
 }
 ?>
